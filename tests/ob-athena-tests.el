9cfbf2a89ce4cf37ade6e47c5dc8f3aeeaf8a1b5
Will Bosch-Bello <williamsbosch@gmail.com>
2025-07-05 22:46:31 -0500

    test: add test to test file

--- tests/ob-athena-tests.el
+++ tests/ob-athena-tests.el
@@ -1,4 +1,4 @@
- ;;; ob-athena-tests.el --- Load all ERT test files for ob-athena -*- lexical-binding: t; -*-
+;;; ob-athena-tests.el --- Load all ERT test files for ob-athena -*- lexical-binding: t; -*-
 
 (defconst ob-athena-test-dir
   (file-name-directory (or load-file-name buffer-file-name)))
@@ -8,6 +8,8 @@
 (load (expand-file-name "ob-athena-cli-query-execution-tests.el" ob-athena-test-dir))
 (load (expand-file-name "ob-athena-minimal-integration-tests.el" ob-athena-test-dir))
 (load (expand-file-name "ob-athena-output-converstion-tests.el" ob-athena-test-dir))
+(load (expand-file-name "ob-athena-full-integration-tests.el" ob-athena-test-dir))
+(load (expand-file-name "ob-athena-no-babel-headers.el" ob-athena-test-dir))
 
 (provide 'ob-athena-tests)
 
